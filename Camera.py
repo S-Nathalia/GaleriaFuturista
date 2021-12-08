26e6b6da3843ef3b142a1da4c948f1c768e48ec8
S-Nathalia <nathysantos1516@gmail.com>
2021-12-08 01:36:30 -0300

    aprimoramento em camera

--- Camera.py
+++ Camera.py
@@ -4,20 +4,20 @@ from math import sin, cos, radians
 class Camera:
     def __init__(self):
         self.camera_pos = Vector3([0.0, 4.0, 3.0])
-        self.camera_front = Vector3([0.0, 0.0, -1.0])
-        self.camera_up = Vector3([0.0, 1.0, 0.0])
-        self.camera_right = Vector3([1.0, 0.0, 0.0])
+        self.camera_frente = Vector3([0.0, 0.0, -1.0])
+        self.camera_cima = Vector3([0.0, 1.0, 0.0])
+        self.camera_direita = Vector3([1.0, 0.0, 0.0])
 
-        self.mouse_sensitivity = 0.25
+        self.mouse_sen = 0.25
         self.jaw = -90
         self.pitch = 0
 
     def get_view_matrix(self):
-        return matrix44.create_look_at(self.camera_pos, self.camera_pos + self.camera_front, self.camera_up)
+        return matrix44.create_look_at(self.camera_pos, self.camera_pos + self.camera_frente, self.camera_cima)
 
     def process_mouse_movement(self, xoffset, yoffset, constrain_pitch=True):
-        xoffset *= self.mouse_sensitivity
-        yoffset *= self.mouse_sensitivity
+        xoffset *= self.mouse_sen
+        yoffset *= self.mouse_sen
 
         self.jaw += xoffset
         self.pitch += yoffset
@@ -31,38 +31,22 @@ class Camera:
         self.update_camera_vectors()
 
     def update_camera_vectors(self):
-        front = Vector3([0.0, 0.0, 0.0])
-        front.x = cos(radians(self.jaw)) * cos(radians(self.pitch))
-        front.y = sin(radians(self.pitch))
-        front.z = sin(radians(self.jaw)) * cos(radians(self.pitch))
+        frente = Vector3([0.0, 0.0, 0.0])
+        frente.x = cos(radians(self.jaw)) * cos(radians(self.pitch))
+        frente.y = sin(radians(self.pitch))
+        frente.z = sin(radians(self.jaw)) * cos(radians(self.pitch))
 
-        self.camera_front = vector.normalise(front)
-        self.camera_right = vector.normalise(vector3.cross(self.camera_front, Vector3([0.0, 1.0, 0.0])))
-        self.camera_up = vector.normalise(vector3.cross(self.camera_right, self.camera_front))
+        self.camera_frente = vector.normalise(frente)
+        self.camera_direita = vector.normalise(vector3.cross(self.camera_frente, Vector3([0.0, 1.0, 0.0])))
+        self.camera_cima = vector.normalise(vector3.cross(self.camera_direita, self.camera_frente))
 
     # Camera method for the WASD movement
-    def process_keyboard(self, direction, velocity):
-        if direction == "FORWARD":
-            self.camera_pos += self.camera_front * velocity
-        if direction == "BACKWARD":
-            self.camera_pos -= self.camera_front * velocity
-        if direction == "LEFT":
-            self.camera_pos -= self.camera_right * velocity
-        if direction == "RIGHT":
-            self.camera_pos += self.camera_right * velocity
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def process_keyboard(self, direcao, velocidade):
+        if direcao == "FORWARD":
+            self.camera_pos += self.camera_frente * velocidade
+        if direcao == "BACKWARD":
+            self.camera_pos -= self.camera_frente * velocidade
+        if direcao == "LEFT":
+            self.camera_pos -= self.camera_direita * velocidade
+        if direcao == "RIGHT":
+            self.camera_pos += self.camera_direita * velocidade
